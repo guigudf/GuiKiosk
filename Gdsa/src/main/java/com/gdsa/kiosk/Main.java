ad8e0a2beba8e4b3a52b6a83ce08ce3e3357bb42
C1323210 Guilherme Dos Santos Araujo <c1323210@interno.bb.com.br>
2025-11-13 20:31:14 -0800

    Week 8-9: Added UI and cart interaction

--- Gdsa/src/main/java/com/gdsa/kiosk/Main.java
+++ Gdsa/src/main/java/com/gdsa/kiosk/Main.java
@@ -1,21 +1,19 @@
 package com.gdsa.kiosk;
 
 import com.gdsa.kiosk.UI.KioskUI;
+import com.gdsa.kiosk.UI.MainFrame;
 
 public class Main {
-//    public static void main(String[] args) {
-//        MenuItem coffee = new MenuItem("Coffee", new BigDecimal("3.99"));
-//
-//        System.out.println(coffee.toString());
-//
-//        String name= "Guilherme";
-//
-//        name.equals("Guilherme");
-//    }
-
-    public static void main() {
-        KioskUI kiosk = new KioskUI();
-        kiosk.setSize(1024, 720);
-        kiosk.setVisible(true);
+    public static void main(String[] args) {
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            MainFrame f = new MainFrame();
+            f.setVisible(true);
+        });
     }
+
+//    public static void main() {
+//        KioskUI kiosk = new KioskUI();
+//        kiosk.setSize(1024, 720);
+//        kiosk.setVisible(true);
+//    }
 }
